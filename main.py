5c2646e02a1d45fa5016bab6f93f2dce273c44ab
openhamster <61841951+openhamster@users.noreply.github.com>
2023-04-02 16:41:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents().all()  # or specify the intents you want to use
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# define a command to send a welcome message to new members
+@bot.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    if channel is not None:
+        await channel.send(f'Welcome {member.mention} to the server!')
+
+# define a command to send a greeting message when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is ready to go!')
+
+# define a command to respond to a user command
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f'Hello, {ctx.author.mention}!')
+
+
+@bot.command(name='kick')
+async def kick(ctx, member: discord.Member, *, reason=None):
+    """Kicks a member from the server."""
+    await member.kick(reason=reason)
+    await ctx.send(f'Member {member} has been kicked.')
+
+@bot.command(name='ban')
+async def ban(ctx, member: discord.Member, *, reason=None):
+    """Bans a member from the server."""
+    await member.ban(reason=reason)
+    await ctx.send(f'Member {member} has been banned.')
+
+@bot.command(name='echo')
+async def echo(ctx, *, message):
+    """Echoes the user's message."""
+    await ctx.send(message)
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'Pong!')
+
+@bot.command(name='info')
+async def info(ctx):
+    embed=discord.Embed(title="Fatare Bot", description="FatareBOT OSC (OSC = Open Source Core) - Its a new version of FatareBot Core, but with more stable code and new features!", color=discord.Color.blue())
+    await ctx.send(embed=embed)
+
+@bot.command(name='clear')
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=250):
+    """Clears a specified number of messages from the channel."""
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f'{amount} сообщений были удалены с канала!')
+
+@bot.command(name='mute')
+async def mute(ctx, member: discord.Member, *, reason=None):
+    """Mutes a member by adding a mute role."""
+    mute_role = discord.utils.get(ctx.guild.roles, name='Muted')  # get the mute role
+    if not mute_role:
+        # if the mute role does not exist, create it
+        mute_role = await ctx.guild.create_role(name='Muted', reason='Mute command')
+        for channel in ctx.guild.channels:
+            # deny send messages permissions for the muted role in all channels
+            await channel.set_permissions(mute_role, send_messages=False)
+    
+    await member.add_roles(mute_role, reason=reason)
+    await ctx.send(f'{member.mention} был замьючен.')
+
+@bot.command(name='unmute')
+@commands.has_permissions(administrator=True)
+async def unmute(ctx, member: discord.Member):
+    muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    await member.remove_roles(muted_role)
+    await ctx.send(f"{member.mention} теперь может говорить.")
+
+
+# run the bot with the token from Discord Developer Portal
+bot.run("#Your token here")
